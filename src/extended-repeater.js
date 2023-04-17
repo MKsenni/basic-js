1794092decfeab74f8cbf24c0452f97ba2471c44
Kseniya <pen.cil@mail.ru>
2023-04-18 04:34:40 +0500

    feat: add slove for task extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = '';
+  let newStr = '';
+
+  let repeatTimes = options.repeatTimes || true;
+  let separator = options.separator || '+';
+  let addition = options.addition;
+  let additionRepeatTimes = options.additionRepeatTimes || true;
+  let additionSeparator = options.additionSeparator || '|';
+
+  if (typeof str !== 'string') toString(str);
+  if (typeof addition !== 'string') {
+    toString(addition);
+  } 
+  if (addition === undefined) {
+    addition = '';
+  }
+    
+  for (let i = 1; i <= additionRepeatTimes; i++) {
+    newStr += addition;
+
+    if (i+1 <= additionRepeatTimes) {
+      newStr += additionSeparator;
+    }
+  }
+
+  for (let i = 1; i <= repeatTimes; i++) {
+    result += str + newStr;
+    if (i+1 <= repeatTimes) {
+      result += separator;
+    }
+  }
+  return result;
 }
 
 module.exports = {
