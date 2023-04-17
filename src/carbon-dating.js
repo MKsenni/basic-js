eb0ba57e4029963d108a01a6b29b621432003aab
Kseniya <pen.cil@mail.ru>
2023-04-18 02:23:23 +0500

    feat: add slove for task carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
   // remove line with error and write your code here
+  if (typeof sampleActivity !== 'string') return false;
+  let num = parseFloat(sampleActivity);
+  if (isNaN(num) || typeof num !== 'number' || num <= 0 || num > MODERN_ACTIVITY) return false;
+  return Math.ceil(Math.log(MODERN_ACTIVITY/num) / 0.693 * HALF_LIFE_PERIOD);
 }
 
 module.exports = {
